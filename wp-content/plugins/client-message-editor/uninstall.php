dec15ebe5a9046a13eb7eee9bdfd0deb7598a2f6
Ancelin Quinton <ancelin.quinton@gmail.com>
2020-08-28 17:00:18 +0200

    maj db et correction plugin uninstall

--- wp-content/plugins/client-message-editor/uninstall.php
+++ wp-content/plugins/client-message-editor/uninstall.php
@@ -9,7 +9,7 @@ defined('WP_UNINSTALL_PLUGIN') || exit;
 
 //clear database 
 
-$clientMessages = get_posts(array('post_type' => 'client_message_editor', 'numberposts' => -1));
+$clientMessages = get_posts(array('post_type' => 'client_message', 'numberposts' => -1));
 
 if ($clientMessages != false) {
   foreach ($clientMessages as $clientMessage) {
